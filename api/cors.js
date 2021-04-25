1d754b794b33bd7358807c5d89e880a3506781c3
alienz <limingxia07@gmail.com>
2021-04-25 11:34:10 +1000

    test cores.js without header

--- api/cors.js
+++ api/cors.js
@@ -1,7 +1,41 @@
 const { send } = require('micro')
 const microCors = require('micro-cors')
+
+// Import Dependencies
+const url = require('url')
+const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
+
 const cors = microCors({ allowMethods: ['PUT', 'POST', 'GET'] })
 
-const handler = (req, res) => send(res, 200, 'ok!')
+
+// Create cached connection variable
+let cachedDb = null
+
+// A function for connecting to MongoDB,
+// taking a single parameter of the connection string
+async function connectToDatabase(uri) {
+    // If the database connection is cached,
+    // use it instead of creating a new connection
+    if (cachedDb) {
+        return cachedDb
+    }
+
+    // If no connection is cached, create a new one
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+
+    // Select the database through the connection,
+    // using the database path of the connection string
+    const db = await client.db(url.parse(uri).pathname.substr(1))
+
+    // Cache the database connection and return the connection
+    cachedDb = db
+    return db
+}
+
+const handler = async (req, res) => {
+    const db = await connectToDatabase(process.env.DB_URI)
+    send(res, 200, 'ok!')
+}
 
 module.exports = cors(handler)
