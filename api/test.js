0fccc0f76022627811d8ba14c1f4d22f0e64fe24
alienz <limingxia07@gmail.com>
2021-04-25 16:30:09 +1000

    try offical example

--- api/test.js
+++ api/test.js
@@ -1,43 +1,23 @@
-const { send } = require('micro')
-const microCors = require('micro-cors')
-
-// Import Dependencies
-const url = require('url')
-const MongoClient = require('mongodb').MongoClient
-
-const cors = microCors({ allowMethods: ['PUT', 'POST', 'GET'] })
-
-
-// Create cached connection variable
-let cachedDb = null
-
-// A function for connecting to MongoDB,
-// taking a single parameter of the connection string
-async function connectToDatabase(uri) {
-    // If the database connection is cached,
-    // use it instead of creating a new connection
-    if (cachedDb) {
-        return cachedDb
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
     }
-
-    // If no connection is cached, create a new one
-    const client = await MongoClient.connect(uri, { useNewUrlParser: true })
-
-    // Select the database through the connection,
-    // using the database path of the connection string
-    const db = await client.db(url.parse(uri).pathname.substr(1))
-
-    // Cache the database connection and return the connection
-    cachedDb = db
-    return db
+    return await fn(req, res)
 }
 
-const handler = async (req, res) => {
-    if (req.method === 'POST') {
-        // const { post: { _id, views, visitedDate, totalDuration } } = req.body
-        // fetch id, update read, like, duration
-        send(res, 200, {data: req.body})
-    }
+const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
 }
 
-module.exports = cors(handler)
+module.exports = allowCors(handler)
